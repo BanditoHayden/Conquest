bec98c984f92dcd143a86b7eabe7c84987c2c4fa
goose <40964328+TDLPoof@users.noreply.github.com>
2023-08-22 01:56:26 -0500

    Energy weapons: part 2

--- Items/Weapons/Magic/Energy/Marksman.cs
+++ Items/Weapons/Magic/Energy/Marksman.cs
@@ -15,7 +15,7 @@ using Mono.Cecil;
 
 namespace Conquest.Items.Weapons.Magic.Energy;
 
-public class Kaliber : EnergyWeapon
+public class Marksman : EnergyWeapon
 {
     SoundStyle RetroBlast = new SoundStyle($"{nameof(Conquest)}/Assets/Sounds/Retro_Blast")
     {
@@ -27,40 +27,39 @@ public class Kaliber : EnergyWeapon
 
 	public override void SetDefaults()
     {
+        Item.scale = 1.5f;
 		Item.width = 26; 
-        Item.height = 13;
+        Item.height = 8;
         Item.value = 1000;
         Item.noMelee = true;
         Item.rare = ItemRarityID.Pink;
         Item.mana = 12;
         Item.useTime = 1;
-        Item.useAnimation = 20;
+        Item.useAnimation = 75;
         Item.useStyle = ItemUseStyleID.Shoot;
         Item.noUseGraphic = false;
-        Item.damage = 26;
-        Item.knockBack = 1f;
+        Item.damage = 100;
+        Item.knockBack = 6f;
         Item.DamageType = DamageClass.Magic;
-		Item.shoot = ModContent.ProjectileType<KaliberBolt>();
+		Item.shoot = ModContent.ProjectileType<MarksmanBolt>();
 		Item.shootSpeed = 8;
 		Item.autoReuse = true;
 	}
 
-    int shotsFired = 0;
-    public override void UpdateInventory(Player player)
+    public override Vector2? HoldoutOffset()
     {
-        float energy = player.GetModPlayer<EnergyPlayer>().energyPower;
-        Item.useTime = 1;
-        Item.useAnimation = (int)MathF.Round(21 - (3 * energy));
-        if (Item.useAnimation < 10) Item.useAnimation = 10;
+        return new Vector2(-8, 0);
     }
+
+    int shotsFired = 0;
 
     public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
     {
         if (shotsFired == 0) SoundEngine.PlaySound(SoundID.Item43, position);
-        if (shotsFired % 3 == 0 && shotsFired < 10)
+        if (shotsFired % 3 == 0 && shotsFired < 20)
         {
             Vector2 oldPos = position;
-            type = ModContent.ProjectileType<KaliberSpawnBolt>();
+            type = ModContent.ProjectileType<MarksmanSpawnBolt>();
             float ringWidth = Main.rand.NextFloat(200f, 220f);
             position += Main.rand.NextVector2CircularEdge(ringWidth, ringWidth);
         }
@@ -71,19 +70,24 @@ public class Kaliber : EnergyWeapon
         else
         {
             SoundEngine.PlaySound(RetroBlast, position);
+            float energy = player.GetModPlayer<EnergyPlayer>().energyPower;
+            for (int i = 0; i < (int)energy - 1; i++)
+            {
+                Projectile.NewProjectileDirect(player.GetSource_ItemUse(Item), position, velocity, type, damage, knockback, player.whoAmI);
+            }
         }
         shotsFired++;
         shotsFired = shotsFired % Item.useAnimation;
     }
 }
 
-public class KaliberSpawning : GlobalNPC
+public class MarksmanSpawning : GlobalNPC
 {
     public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
     {
-        if (npc.type == NPCID.TheDestroyer)
+        if (npc.type == NPCID.SkeletronPrime)
         {
-            npcLoot.Add(ItemDropRule.NormalvsExpert(ModContent.ItemType<Kaliber>(), 5, 3));
+            npcLoot.Add(ItemDropRule.NormalvsExpert(ModContent.ItemType<Marksman>(), 5, 3));
         }
     }
 }
