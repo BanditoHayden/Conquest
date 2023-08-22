6e057075f0b58623fd889d396471d0a1048a617d
goose <40964328+TDLPoof@users.noreply.github.com>
2023-08-21 21:52:58 -0500

    Energy weapons: the beginning
    Beginning of the Energy subclass of Mage. This commit adds the projectiles used by the Kaliber.

---
+++ Projectiles/Magic/Energy/KaliberBolt.cs
@@ -0,0 +1,47 @@
+﻿
+using Conquest.Assets.Common;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Conquest.Projectiles.Magic.Energy;
+
+public class KaliberBolt : ModProjectile
+{
+    public override void SetStaticDefaults()
+    {
+        ProjectileID.Sets.TrailCacheLength[Projectile.type] = 50;    //The length of old position to be recorded
+        ProjectileID.Sets.TrailingMode[Projectile.type] = 3;
+    }
+    public override void SetDefaults()
+    {
+        Projectile.width = 8;
+        Projectile.height = 8;
+        Projectile.aiStyle = 1;
+        Projectile.friendly = true;
+        Projectile.DamageType = DamageClass.Magic;
+        Projectile.penetrate = 3;
+        Projectile.timeLeft = 900;
+        Projectile.alpha = 255;
+        Projectile.light = 0.5f;
+        Projectile.ignoreWater = true;
+        Projectile.tileCollide = true;
+        Projectile.extraUpdates = 3;
+        Projectile.alpha = 255;
+        AIType = ProjectileID.Bullet;
+    }
+    public override bool PreDraw(ref Color lightColor)
+    {
+        default(Effects.PurpleTrailSmall).Draw(Projectile);
+
+        return false;
+    }
+
+}
