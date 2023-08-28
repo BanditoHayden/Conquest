62b2e8a8b05547371c667ce85a69813f854e2c7b
Moddingus <106451891+Moddingus@users.noreply.github.com>
2023-08-28 21:49:28 +0800

    Melee Projectiles

---
+++ Projectiles/Melee/Halo.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Conquest.Projectiles.Melee
+{
+    public class Halo : ModProjectile
+    {
+        private ref float scale => ref Projectile.ai[0]; //adjust scale for a pulse effect
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 40;
+            Projectile.height = 40;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.penetrate = 1;
+            Projectile.aiStyle = -1;
+            Projectile.timeLeft = 300;
+            Projectile.alpha = 0;
+            Projectile.light = 0;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = false;
+        }
+        public override void AI()
+        {
+            
+            base.AI();
+        }
+    }
+}
+
