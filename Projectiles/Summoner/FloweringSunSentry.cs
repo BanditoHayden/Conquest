4ea127afbd808c293e36ca18cc43b83949e9ac5a
goose <40964328+TDLPoof@users.noreply.github.com>
2023-09-05 00:07:20 -0500

    Living Shadow of coolness
    Added the projectiles for Gamma Ray and Echoing Starlash

---
+++ Projectiles/Summoner/FloweringSunSentry.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+using Microsoft.Xna.Framework;
+using Conquest.Assets.Common;
+
+namespace Conquest.Projectiles.Summoner;
+
+public class FloweringSunSentry : ModProjectile
+{
+    public override void SetDefaults()
+    {
+        Projectile.width = 15;
+        Projectile.height = 24;
+        Projectile.scale = 1f;
+        Projectile.DamageType = DamageClass.Summon;
+        Projectile.penetrate = -1;
+        Projectile.sentry = true;
+    }
+
+    public override bool OnTileCollide(Vector2 oldVelocity)
+    {
+        return false;
+    }
+
+    public override void AI()
+    {
+        Projectile.velocity.Y += 12;
+
+        if (Projectile.ai[0] == 0) Projectile.ai[0] = (60 * 22);
+
+        if (Projectile.ai[0]++ % (60 * 24) == 0)
+        {
+            SoundEngine.PlaySound(SoundID.Item29, Projectile.position);
+            Projectile.NewProjectileDirect(Projectile.GetSource_FromThis(),
+                Projectile.position, Main.rand.NextVector2CircularEdge(15, 15),
+                ModContent.ProjectileType<HealingSun>(), 0, 0, Projectile.owner);
+        }
+    }
+}
+
---
+++ Projectiles/Summoner/HealingSun.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Conquest.Assets.Common;
+
+namespace Conquest.Projectiles.Summoner;
+
+internal class HealingSun : ModProjectile
+{
+    public override void SetDefaults()
+    {
+        Projectile.width = 10;
+        Projectile.height = 10;
+        Projectile.friendly = false;
+        Projectile.hostile = false;
+        Projectile.timeLeft = 450;
+        Projectile.light = 1f;
+        Projectile.ignoreWater = true;
+        Projectile.tileCollide = false;
+    }
+    public override void AI()
+    {
+        float rotationBonus = Projectile.velocity.Length();
+        rotationBonus *= (Projectile.velocity.X > 0) ? 1 : -1;
+        Projectile.rotation += MathHelper.ToRadians(rotationBonus);
+        Projectile.velocity *= 0.94f;
+
+        foreach (Player player in Main.player)
+        {
+            if (!player.active || player.dead || player.Distance(Projectile.Center) > 300) continue;
+            if (player.Hitbox.Intersects(Projectile.Hitbox))
+            {
+                player.Heal(50);
+                Projectile.Kill();
+            }
+            Projectile.velocity = Vector2.Lerp(Projectile.velocity, Projectile.DirectionTo(player.Center) * 8, 0.11f);
+        }
+
+    }
+}
