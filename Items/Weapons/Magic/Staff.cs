749c7c4eab5a52348ee2df63b4e6dcb765d87e58
Moddingus <106451891+Moddingus@users.noreply.github.com>
2023-09-10 21:48:35 +0800

    some staffs
    these need to be renamed

---
+++ Items/Weapons/Magic/Staff.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using Conquest.Projectiles.Magic;
+using Microsoft.CodeAnalysis;
+
+namespace Conquest.Items.Weapons.Magic
+{
+    public class Staff : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 1;
+            // Tooltip.SetDefault("Projectiles fire at your cursor, Critical hits heal you");
+            Item.staff[Item.type] = true;
+        }
+        public override void SetDefaults()
+        {
+            // Common Properties
+            Item.width = 36;
+            Item.height = 36;
+            Item.value = Item.sellPrice(silver: 1);
+            Item.noMelee = true;
+            Item.rare = 6;
+            // Use Properties
+            Item.useTime = 15;
+            Item.useAnimation = 15;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.noUseGraphic = false;
+            Item.UseSound = SoundID.Item43;
+            Item.autoReuse = true;
+            // Weapon Properties
+            Item.damage = 30;
+            Item.knockBack = 3f;
+            Item.DamageType = DamageClass.Magic;
+            Item.mana = 9;
+            // Projectile Properties
+            Item.shoot = ModContent.ProjectileType<StaffProjectile>();
+            Item.shootSpeed = 12f;
+        }
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            Vector2 muzzleOffset = Vector2.Normalize(velocity) * 55f;
+
+            if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+            {
+                position += muzzleOffset;
+            }
+            base.ModifyShootStats(player, ref position, ref velocity, ref type, ref damage, ref knockback);
+        }
+    }
+    public class StaffProjectile : ModProjectile
+    {
+        private ref float timer => ref Projectile.ai[0];
+        private ref float otherTimer => ref Projectile.ai[1];
+        public override string Texture => $"Terraria/Images/Projectile_{ProjectileID.AmethystBolt}";
+        public override void SetDefaults()
+        {
+            Projectile.ignoreWater = true;
+            Projectile.width = 20;
+            Projectile.height = 20;
+            Projectile.penetrate = -1;
+            Projectile.friendly = true;
+            Projectile.tileCollide = true;
+            Projectile.aiStyle = -1;
+            Projectile.Opacity = 0;
+            Projectile.timeLeft = 360;
+            base.SetDefaults();
+        }
+        public override void AI()
+        {
+            timer++;
+            ++otherTimer;
+            if (otherTimer >= 20)
+            {
+                otherTimer = 1;
+            }
+            var pos1 = Projectile.Center + Vector2.One.RotatedBy(timer / 10) * 10;
+            var pos2 = Projectile.Center + Vector2.One.RotatedBy(timer / 10 + MathHelper.Pi) * 10;
+            Dust a = Dust.NewDustPerfect(pos1, DustID.GemSapphire, Vector2.Zero, (int)(255 - timer * 17), Color.LightBlue);
+            Dust b = Dust.NewDustPerfect(pos2, DustID.GemSapphire, Vector2.Zero, (int)(255 - timer * 17), Color.LightBlue);
+            a.noGravity = true;
+            b.noGravity = true;
+
+
+            //i was gonna make a lurch effect on this but couldnt get it to look good so i didnt
+            base.AI();
+        }
+    }
+}
---
+++ Items/Weapons/Magic/Staff2.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using Conquest.Projectiles.Magic;
+
+namespace Conquest.Items.Weapons.Magic
+{
+    public class Staff2 : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 1;
+            // Tooltip.SetDefault("Projectiles fire at your cursor, Critical hits heal you");
+            Item.staff[Item.type] = true;
+        }
+        public override void SetDefaults()
+        {
+            // Common Properties
+            Item.width = 30;
+            Item.height = 30;
+            Item.value = Item.sellPrice(silver: 1);
+            Item.noMelee = true;
+            Item.rare = 6;
+            // Use Properties
+            Item.useTime = 40;
+            Item.useAnimation = 40;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.noUseGraphic = false;
+            Item.UseSound = SoundID.Item43;
+            Item.autoReuse = true;
+            // Weapon Properties
+            Item.damage = 30;
+            Item.knockBack = 3f;
+            Item.DamageType = DamageClass.Magic;
+            Item.mana = 9;
+            // Projectile Properties
+            Item.shoot = ModContent.ProjectileType<Staff2Projectile>();
+            Item.shootSpeed = 12f;
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Vector2 muzzleOffset = Vector2.Normalize(velocity) * 60f;
+
+            if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+            {
+                position += muzzleOffset;
+            }
+            float rot = MathHelper.PiOver4 / 4;
+            int num = 4;
+
+            for (float i = 0; i < num; i++)
+            {
+                Vector2 perturbedSpeed = velocity.RotatedBy(MathHelper.Lerp(-rot, rot, i / (float)(num - 1))); // Watch out for dividing by 0 if there is only 1 projectile.
+                Projectile.NewProjectile(source, position, perturbedSpeed, type, damage, knockback, player.whoAmI);
+            }
+
+            return false;
+
+        }
+    }
+    public class Staff2Projectile : ModProjectile
+    {
+        private ref float timer => ref Projectile.ai[0];
+        public override string Texture => $"Terraria/Images/Projectile_{ProjectileID.AmethystBolt}";
+        public override void SetDefaults()
+        {
+            Projectile.ignoreWater = true;
+            Projectile.width = 4;
+            Projectile.height = 4;
+            Projectile.penetrate = 5;
+            Projectile.friendly = true;
+            Projectile.tileCollide = true;
+            Projectile.aiStyle = -1;
+            Projectile.Opacity = 0;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 5;
+            base.SetDefaults();
+        }
+        float gravity = 0.2f;
+        public override void AI()
+        {
+            timer++;
+            Dust a = Dust.NewDustPerfect(Projectile.Center, DustID.GemAmethyst, Vector2.Zero, (int)(255 - timer * 17));
+            a.noGravity = true;
+
+            Projectile.velocity.Y += gravity;
+            base.AI();
+        }
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            if (Projectile.velocity.X != oldVelocity.X)
+            {
+                Projectile.velocity.X = -oldVelocity.X;
+            }
+            if (Projectile.velocity.Y != oldVelocity.Y)
+            {
+                Projectile.velocity.Y = -oldVelocity.Y;
+            }
+            return false;
+        }
+    }
+}
