c2e480583905b40c380bc0297e82e198a34f5b31
goose <40964328+TDLPoof@users.noreply.github.com>
2023-09-13 15:41:27 -0500

    Update Effects.cs
    Add Reflector trails

--- Assets/Common/Effects.cs
+++ Assets/Common/Effects.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -502,6 +502,70 @@ namespace Conquest.Assets.Common
             private float StripWidth(float progressOnStrip)
             {
                 float num = 0.2f;
+                float lerpValue = Utils.GetLerpValue(-0f, 0.2f, progressOnStrip, clamped: true);
+                num *= 1f - (1f - lerpValue) * (1f - lerpValue);
+                return MathHelper.Lerp(0f, 32f, num);
+            }
+        }
+
+        public struct BlackTrailRotationless
+        {
+            private static VertexStrip _vertexStrip = new VertexStrip();
+
+            public void Draw(Projectile proj)
+            {
+                MiscShaderData miscShaderData = GameShaders.Misc["MagicMissile"];
+                miscShaderData.UseSaturation(-2.8f);
+                miscShaderData.UseOpacity(4f);
+                miscShaderData.Apply();
+                _vertexStrip.PrepareStripWithProceduralPadding(proj.oldPos, proj.oldRot, StripColors, StripWidth, -Main.screenPosition + proj.Size / 2f);
+                _vertexStrip.DrawTrail();
+                Main.pixelShader.CurrentTechnique.Passes[0].Apply();
+            }
+
+            private Color StripColors(float progressOnStrip)
+            {
+                Color value = Main.hslToRgb(.225f, .39f, 0.7f);
+                Color result = Color.Black;
+                result.A = 0;
+                return result;
+            }
+
+            private float StripWidth(float progressOnStrip)
+            {
+                float num = 1f;
+                float lerpValue = Utils.GetLerpValue(-0f, 0.2f, progressOnStrip, clamped: true);
+                num *= 1f - (1f - lerpValue) * (1f - lerpValue);
+                return MathHelper.Lerp(0f, 32f, num);
+            }
+        }
+
+        public struct WhiteTrailRotationless
+        {
+            private static VertexStrip _vertexStrip = new VertexStrip();
+
+            public void Draw(Projectile proj)
+            {
+                MiscShaderData miscShaderData = GameShaders.Misc["MagicMissile"];
+                miscShaderData.UseSaturation(-2.8f);
+                miscShaderData.UseOpacity(4f);
+                miscShaderData.Apply();
+                _vertexStrip.PrepareStripWithProceduralPadding(proj.oldPos, proj.oldRot, StripColors, StripWidth, -Main.screenPosition + proj.Size / 2f);
+                _vertexStrip.DrawTrail();
+                Main.pixelShader.CurrentTechnique.Passes[0].Apply();
+            }
+
+            private Color StripColors(float progressOnStrip)
+            {
+                Color value = Main.hslToRgb(.225f, .39f, 0.7f);
+                Color result = Color.White;
+                result.A = 0;
+                return result;
+            }
+
+            private float StripWidth(float progressOnStrip)
+            {
+                float num = 1f;
                 float lerpValue = Utils.GetLerpValue(-0f, 0.2f, progressOnStrip, clamped: true);
                 num *= 1f - (1f - lerpValue) * (1f - lerpValue);
                 return MathHelper.Lerp(0f, 32f, num);
